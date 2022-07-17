22d06537a48a6aa6089e4e07bbe3571410b4b87d
Josue Dario Alfaro <analista_sistemas4@soft-net.com.pe>
2022-07-17 17:47:32 -0300

    primer commit

---
+++ src/main/java/sistema/manejo/demo/DeltaApplication.java
@@ -0,0 +1,11 @@
+package sistema.manejo.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeltaApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(DeltaApplication.class, args);
+	}
+}
---
+++ src/test/java/sistema/manejo/demo/DeltaApplicationTests.java
@@ -0,0 +1,13 @@
+package sistema.manejo.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeltaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
