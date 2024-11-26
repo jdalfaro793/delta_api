e87163c0b92fff682d1ed512fae23944d1d57bae
Josué Darío Alfaro <darioalfarounju@gmail.com>
2024-11-26 16:50:44 -0300

    añadiendo configuraciones para realizar test

--- src/test/java/sistema/manejo/demo/DeltaApplicationTests.java
+++ src/test/java/sistema/manejo/demo/DeltaApplicationTests.java
@@ -1,13 +1,20 @@
 package sistema.manejo.demo;
 
-import org.junit.jupiter.api.Test;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
 
-@SpringBootTest
-class DeltaApplicationTests {
+@SpringBootTest(classes = {DeltaApplication.class})
+@RunWith(SpringRunner.class)
+public class DeltaApplicationTests {
+	private static final Logger log = LoggerFactory.getLogger(DeltaApplicationTests.class);
 
 	@Test
-	void contextLoads() {
+	public void contextLoads() {
+		log.info("Ejecutando contextLoads()");
 	}
 
 }
