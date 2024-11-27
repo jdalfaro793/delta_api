2ea1e9944b72068a870d6e4860f1ad15a832e79f
Josué Darío Alfaro <darioalfarounju@gmail.com>
2024-11-27 11:20:20 -0300

    add cambios

--- src/main/java/sistema/manejo/demo/controller/RoleController.java
+++ src/main/java/sistema/manejo/demo/controller/RoleController.java
@@ -1,6 +1,5 @@
 package sistema.manejo.demo.controller;
 
-import java.util.Date;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -8,18 +7,17 @@ import java.util.Map;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import sistema.manejo.demo.dto.RoleDTO;
 import sistema.manejo.demo.model.Role;
 import sistema.manejo.demo.service.RoleService;
-
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 
 
 @RestController
